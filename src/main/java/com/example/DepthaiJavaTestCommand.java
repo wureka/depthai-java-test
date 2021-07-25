a922cef56066d4575dc5c35f4ae9a3e680728140
wureka <wureka@gmail.com>
2021-07-25 17:57:40 +0800

    init

--- src/main/java/com/example/DepthaiJavaTestCommand.java
+++ src/main/java/com/example/DepthaiJavaTestCommand.java
@@ -3,8 +3,8 @@ package com.example;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.context.ApplicationContext;
 
-import org.bytedeco.depthai.Device;
-import org.bytedeco.depthai.Pipeline;
+import org.bytedeco.depthai.*;
+import org.bytedeco.javacpp.IntPointer;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -27,7 +27,22 @@ public class DepthaiJavaTestCommand implements Runnable {
             System.out.println("Hi!");
         }
         Pipeline pipeline = new Pipeline();
-        Device device = new Device();
+        ColorCamera camRgb = pipeline.createColorCamera();
+        XLinkOut xoutRgb   = pipeline.createXLinkOut();
+        xoutRgb.setStreamName("rgb");
+        camRgb.setResolution(ColorCameraProperties.SensorResolution.THE_4_K);
+        camRgb.setPreviewSize(300, 300);
+        camRgb.setInterleaved(false);
+        camRgb.setColorOrder(ColorCameraProperties.ColorOrder.BGR);
 
+        camRgb.preview().link(xoutRgb.input());
+        Device device = new Device(pipeline, false);
+        IntPointer cameras = device.getConnectedCameras();
+        System.out.printf("Detect %d camera(s), USB speed: %s \n", cameras.limit(), device.getUsbSpeed());
+        for (int i = 0; i < cameras.limit(); i++) {
+            System.out.printf("    Camera %d is ready!\n", cameras.get(i));
+        }
+        device.close();
+        pipeline.close();
     }
 }
